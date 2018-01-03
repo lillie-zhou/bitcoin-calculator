5a1c204c782f067acc1d06a56c284e740dd71d9e
Lillie Zhou <zlillie@Lillies-MacBook-Pro.local>
2018-01-02 22:31:46 -0500

    Requests real-time data from BitcoinAverage API

--- BitcoinCalculator/ViewController.swift
+++ BitcoinCalculator/ViewController.swift
@@ -7,19 +7,66 @@
 //
 
 import UIKit
+import SwiftyJSON
+import Alamofire
 
-class ViewController: UIViewController {
+class ViewController: UIViewController , UIPickerViewDataSource, UIPickerViewDelegate {
+    
+    let url = "https://apiv2.bitcoinaverage.com/indices/global/ticker/BTC"
+    let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
+    let currencySymbolArray = ["$", "R$", "$", "¥", "€", "£", "$", "Rp", "₪", "₹", "¥", "$", "kr", "$", "zł", "lei", "₽", "kr", "$", "$", "R"]
+    var selectedCurrencyIndex = 0
 
+    @IBOutlet weak var priceLabel: UILabel!
+    @IBOutlet weak var currencyPicker: UIPickerView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        currencyPicker.delegate = self
+        currencyPicker.dataSource = self
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    // Send an HTTP request to BitcoinAverage API
+    func getCurrency(url: String) {
+        Alamofire.request(url, method: .get).responseJSON { response in
+            if response.result.isSuccess {
+                let currencyJSON: JSON = JSON(response.result.value!)
+                self.parse(json: currencyJSON)
+            } else {
+                self.priceLabel.text = "Conversion Unsuccessful"
+            }
+        }
     }
-
-
+    
+    // Parse the JSON response
+    func parse(json: JSON) {
+        if let convertedPrice = json["ask"].double {
+            let selectedCurrencySymbol = currencySymbolArray[selectedCurrencyIndex]
+            priceLabel.text = "\(selectedCurrencySymbol)\(convertedPrice)"
+        }
+    }
+    
+    // Define # of columns in PickerView
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    // Define # of rows in PickerView
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return currencyArray.count
+    }
+    
+    // Each row in the PickerView is set to a unique currency
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return currencyArray[row]
+    }
+    
+    // If a row is selected, call the getCurrency method
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        let finalURL = url + currencyArray[row]
+        selectedCurrencyIndex = row
+        getCurrency(url: finalURL)
+    }
+    
 }
 
